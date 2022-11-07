8fa153439767fbc5143bd762a8dd851a416cc450
blessedPD <115356350+blessedPD@users.noreply.github.com>
2022-11-07 16:21:07 +0900

    React : Coin App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event)=>{
-    event.preventDefault();
-    if (toDo===""){
-      return;
-    }
-    setToDos(currentArray => [toDo,...currentArray]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [seed,setSeed] = useState(0);
+  const onInputChange = (event) => {
+    setSeed(event.target.value);
   };
-  
+
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json)=> setCoins(json));
+    setLoading(false);
+  },[]);
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length}) </h1>
-
-      <form onSubmit = {onSubmit}>
-        <input value = {toDo} type="text" 
-        onChange = {onChange} placeholder="Write your to do..." />
-        <button>Add to Do</button>
-      </form>
-
+      <h1>The Coins! {loading ? "" : `( ${coins.length} ) `}</h1>
+      <input value={seed} type="number" placeholder="write your seed money!!"
+      onChange={onInputChange} />
+      <b>You entered... {seed}$</b>
       <hr />
-      <ul>
-        {toDos.map((item, index)=> 
-        <li key={index}>{item}</li>)}
-      </ul>
+
+      {loading ? (<strong>Loading</strong>)
+      :
+      (<select>
+        {coins.map((coin)=> 
+        <option>
+          {coin.name} -- You can buy {coin.quotes.USD.price/seed} 
+        </option>)
+        }
+      </select>)
+      }
       
     </div>
-
   );
 }
 
