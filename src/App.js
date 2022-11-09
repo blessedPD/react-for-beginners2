2cd00738a435712d8b71dfe9178d67a6049473be
blessedPD <115356350+blessedPD@users.noreply.github.com>
2022-11-09 16:48:53 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import {
   Switch,
   Routes,
   Route,
-  Link
+  Link,
 }from "react-router-dom";
 import Home from"./routes/Home";
 import Detail from"./routes/Detail";
 
 function App() {
   return( 
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path={`${process.env.PUBLIC_URL}/`}  element={<Home />}/>
       <Route path={`${process.env.PUBLIC_URL}/Movie/:id"`} element={<Detail />}/>
